bcd59e7cf192687e4c38d279cc915280dfb7fb48
vitaliksep <vitaliksep@yandex.ru>
2026-05-06 08:12:10 +0500

    Create layout.jsx

---
+++ src/app/gk-belye-nochi/layout.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "ГК Белые Ночи | Wellness-проект в Сочи - Shirokov Deus Capital",
+  description:
+    "Wellness-проект в п. Уч-Дере, Сочи. Доходность 30-42%, порог входа от 8,2 млн ₽. Клинико-диагностический центр LeePrime Medical, премиальные номера, собственный пляж. Рассрочка 0%. Сдача 3/2027.",
+  openGraph: {
+    title: "ГК Белые Ночи - Wellness-пространство в Сочи",
+    description: "Курортный комплекс с медицинским центром. Доходность до 42%",
+    images: ["https://ucarecdn.com/2dd81fb7-3d08-4a17-99d6-b09412dd0c60/"],
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
