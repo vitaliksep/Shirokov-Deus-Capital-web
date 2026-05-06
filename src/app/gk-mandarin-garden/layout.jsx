adae4b9024bed46751151174262e46c2113506f0
vitaliksep <vitaliksep@yandex.ru>
2026-05-06 08:15:16 +0500

    Create layout.jsx

---
+++ src/app/gk-mandarin-garden/layout.jsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title:
+    "ГК Mandarin Garden | Премиум-комплекс в центре Адлера - Shirokov Deus Capital",
+  description:
+    "Премиум-проект в центре Адлера рядом с ТРЦ Мандарин и морем. Доходность 28-38%, порог входа от 18,7 млн ₽. Элитный курорт круглый год. Рассрочка, ипотека. Старт закрытых продаж. Сдача 1/2030.",
+  openGraph: {
+    title: "ГК Mandarin Garden - Элитный курорт в центре Адлера",
+    description: "Премиум-класс с развитой инфраструктурой. Доходность до 38%",
+    images: ["https://ucarecdn.com/196f5446-85be-48b3-9b93-97ad2757da72/"],
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
