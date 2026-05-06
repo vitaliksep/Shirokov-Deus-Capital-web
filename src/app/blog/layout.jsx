69a4fab1dff8f716beb91bec3bddda4446763a62
vitaliksep <vitaliksep@yandex.ru>
2026-05-06 08:11:28 +0500

    Create layout.jsx

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,24 @@
+export const metadata = {
+  title: "Блог об инвестициях в недвижимость 2026 | Shirokov Deus Capital",
+  description:
+    "Экспертный блог о доходной курортной недвижимости: аналитика рынка, кейсы инвесторов, советы по выбору объектов. Узнайте, как получить 30-45% годовых на побережье Чёрного моря.",
+  keywords:
+    "инвестиции в недвижимость, курортная недвижимость 2026, доходная недвижимость Сочи, апартаменты у моря, пассивный доход недвижимость",
+  openGraph: {
+    title:
+      "Блог об инвестициях в курортную недвижимость | Shirokov Deus Capital",
+    description:
+      "Аналитика, кейсы и экспертные статьи о доходной недвижимости на Черноморском побережье. Доходность 29-45% годовых.",
+    type: "website",
+    images: [
+      "https://raw.createusercontent.com/d7e5f69a-8c91-49a8-97e0-a236258ae198/",
+    ],
+  },
+  alternates: {
+    canonical: "https://shirokov-deus-capital.created.app/blog",
+  },
+};
+
+export default function BlogLayout({ children }) {
+  return children;
+}
