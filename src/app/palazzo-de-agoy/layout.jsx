478bd800628cfd23c8beae140c4c9692267ca81c
vitaliksep <vitaliksep@yandex.ru>
2026-05-06 08:15:30 +0500

    Create layout.jsx

---
+++ src/app/palazzo-de-agoy/layout.jsx
@@ -0,0 +1,17 @@
+export const metadata = {
+  title:
+    "Палаццо Де Агой | Премиум-класс от Versace, Bugatti, Dolce & Gabbana - Shirokov Deus Capital",
+  description:
+    "Уникальный проект с дизайном от Versace, Bugatti и Dolce & Gabbana в п. Агой. Доходность 32-45%, порог входа от 5,9 млн ₽. Первая береговая, SPA-центр, бассейны с подогревом. Старт закрытых продаж. Сдача 2/2027.",
+  openGraph: {
+    title:
+      "Палаццо Де Агой - Совместный проект с Versace, Bugatti, Dolce & Gabbana",
+    description:
+      "Премиальный гостиничный комплекс с эксклюзивным дизайном. Доходность до 45%",
+    images: ["https://ucarecdn.com/ba266c6d-0c4d-44fe-a819-49f604a6fdb9/"],
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
