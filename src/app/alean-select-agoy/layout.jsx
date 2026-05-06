aab8e7af862992678901878e684fee1039026049
vitaliksep <vitaliksep@yandex.ru>
2026-05-06 08:10:39 +0500

    Create layout.jsx

---
+++ src/app/alean-select-agoy/layout.jsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title:
+    "ГК Alean Select Agoy | Инвестиции в курортную недвижимость - Shirokov Deus Capital",
+  description:
+    "Курортный отель для семейного отдыха в п. Агой. Доходность 29-40%, порог входа от 4,7 млн ₽. Первая береговая линия, собственный пляж, акватермальный комплекс. Рассрочка 0%, ипотека. Сдача 1/2029.",
+  openGraph: {
+    title: "ГК Alean Select Agoy - Премиальный курортный комплекс",
+    description:
+      "Инвестиции в апартаменты на первой береговой линии. Доходность до 40%",
+    images: ["https://ucarecdn.com/68df33eb-507c-413c-8c76-fe7274d80aec/"],
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
