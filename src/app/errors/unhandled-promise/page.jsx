e7210021601947462577c30a3b3b9f33690ef86c
vitaliksep <vitaliksep@yandex.ru>
2026-05-08 17:55:54 +0500

    Update page.jsx

--- src/app/errors/unhandled-promise/page.jsx
+++ src/app/errors/unhandled-promise/page.jsx
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 "use client";
 import { useEffect } from "react";
+=======
+'use client';
+import { useEffect } from 'react';
+>>>>>>> 704a5b5d00475b279f7dc72c74c8b05ce837d5dd
 
 // Тестовая страница: намеренно вызывает необработанное отклонение Promise
 // Используется для проверки работы error boundary / мониторинга ошибок
