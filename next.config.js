4ea197acbd1b4f42c1b66f3ffc3936ab9b475160
vitaliksep <vitaliksep@yandex.ru>
2026-05-08 13:29:28 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  // rewrites больше не нужны
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
+  // rewrites удалены: яндекс-верификация обрабатывается через
+  // /src/app/yandex_8d56ecca5f15b2ac.html/page.jsx
 };
 
 module.exports = nextConfig;
