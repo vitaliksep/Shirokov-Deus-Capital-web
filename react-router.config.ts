8ddea69c63e48b6b476479e2727ad43600d81899
vitaliksep <vitaliksep@yandex.ru>
2026-05-13 14:56:28 +0500

    Disable SSR - use client-side only

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,5 @@
-import type { Config } from '@react-router/dev/config';
+﻿import type { Config } from "@react-router/dev/config";
 
 export default {
-	appDirectory: './src/app',
-	ssr: true,
-	prerender: ['/*?'],
+  ssr: false,
 } satisfies Config;
