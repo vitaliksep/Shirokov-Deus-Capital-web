2371174830dfdfe97019e651c474508a444d4e1e
vitaliksep <vitaliksep@yandex.ru>
2026-05-13 21:01:00 +0500

    Fix not-found.tsx export for SPA mode

--- src/app/__create/not-found.tsx
+++ src/app/__create/not-found.tsx
@@ -1,3 +1,10 @@
-﻿export default function NotFound() {
-  return <div>Not Found</div>;
+﻿import { Outlet } from "react-router";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Not Found</h1>
+      <Outlet />
+    </div>
+  );
 }
